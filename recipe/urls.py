e02509dbf646d96b79d191298388bd010adeffa4
RanishBhattarai0 <ranish.28432@student.trinity.edu.np>
2025-10-06 17:40:50 +0545

    Used Tailwinf for more attractive website

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('recipe/', views.recipe_list, name='recipe_list'),
+    path('', views.recipe_list, name='recipe_list'),
     path('recipe/create/', views.recipe_create, name='recipe_create'),
     path('recipe/edit/<int:recipe_id>/', views.recipe_edit, name='edit_recipe'),
     path('recipe/delete/<int:recipe_id>/', views.recipe_delete, name='delete_recipe'),
